6ef7bc47dfc9a5dffaee0702b336332010dd83c4
ly <1095305853@qq.com>
2021-12-13 09:41:01 +0800

    mybatisplus

--- src/main/java/com/liuyi/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/liuyi/springbootdemo/SpringbootDemoApplication.java
@@ -1,25 +1,11 @@
 package com.liuyi.springbootdemo;
 
-import com.liuyi.springbootdemo.mybatis.CityMapper;
-import com.liuyi.springbootdemo.mybatis.User;
-import org.springframework.boot.CommandLineRunner;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
-public class SpringbootDemoApplication implements CommandLineRunner {
-
-    private final CityMapper cityMapper;
-
-    public SpringbootDemoApplication(CityMapper cityMapper) {
-        this.cityMapper = cityMapper;
-    }
-
+@MapperScan("com.liuyi.springbootdemo.mybatisplus.mapper")
+public class SpringbootDemoApplication {
     public static void main(String[] args) { SpringApplication.run(SpringbootDemoApplication.class, args);}
 
-    @Override
-    public void run(String... args) throws Exception {
-        User byState = cityMapper.findByState("1");
-        System.out.println(byState);
-
-    }
 }
--- src/main/java/com/liuyi/springbootdemo/exercise/quartz/SaticScheduleTask.java
+++ src/main/java/com/liuyi/springbootdemo/exercise/quartz/SaticScheduleTask.java
@@ -1,25 +1,25 @@
-package com.liuyi.springbootdemo.exercise.quartz;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-
-import java.time.LocalDateTime;
-
-@Configuration      //1.主要用于标记配置类，兼备Component的效果。
-@EnableScheduling   // 2.开启定时任务
-public class SaticScheduleTask {
-
-    @Autowired
-    private TestAuto testAuto;
-    //3.添加定时任务
-    @Scheduled(cron = "${cron}")
-    //或直接指定时间间隔，例如：5秒
-    //@Scheduled(fixedRate=time)
-    private void configureTasks() {
-        System.err.println("执行静态定时任务时间: " + LocalDateTime.now());
-        testAuto.test();
-    }
-}
+//package com.liuyi.springbootdemo.exercise.quartz;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.Scheduled;
+//
+//import java.time.LocalDateTime;
+//
+//@Configuration      //1.主要用于标记配置类，兼备Component的效果。
+//@EnableScheduling   // 2.开启定时任务
+//public class SaticScheduleTask {
+//
+//    @Autowired
+//    private TestAuto testAuto;
+//    //3.添加定时任务
+//    @Scheduled(cron = "${cron}")
+//    //或直接指定时间间隔，例如：5秒
+//    //@Scheduled(fixedRate=time)
+//    private void configureTasks() {
+//        System.err.println("执行静态定时任务时间: " + LocalDateTime.now());
+//        testAuto.test();
+//    }
+//}
