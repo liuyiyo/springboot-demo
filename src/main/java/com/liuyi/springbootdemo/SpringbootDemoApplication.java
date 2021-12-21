93265f6fac3b4763b41a8ed3f36001b6f89e3661
liuyi <1095305853@qq.com>
2021-12-21 13:27:55 +0800

    mqtt

--- src/main/java/com/liuyi/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/liuyi/springbootdemo/SpringbootDemoApplication.java
@@ -3,24 +3,22 @@ package com.liuyi.springbootdemo;
 import org.mybatis.spring.annotation.MapperScan;
 import com.liuyi.springbootdemo.mqtt.MqttConsumer;
 import com.liuyi.springbootdemo.mybatis.CityMapper;
-import com.liuyi.springbootdemo.mybatis.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.liuyi.springbootdemo.mybatisplus.mapper")
-public class SpringbootDemoApplication {
 public class SpringbootDemoApplication implements CommandLineRunner {
 
     @Autowired
     private MqttConsumer mqttConsumer;
 
-    private final CityMapper cityMapper;
+//    private final CityMapper cityMapper;
 
-    public SpringbootDemoApplication(CityMapper cityMapper) {
-        this.cityMapper = cityMapper;
-    }
+//    public SpringbootDemoApplication(CityMapper cityMapper) {
+//        this.cityMapper = cityMapper;
+//    }
 
     public static void main(String[] args) { SpringApplication.run(SpringbootDemoApplication.class, args);}
 
--- src/main/java/com/liuyi/springbootdemo/mybatisplus/controller/MybatisPlusController.java
+++ src/main/java/com/liuyi/springbootdemo/mybatisplus/controller/MybatisPlusController.java
@@ -2,7 +2,6 @@ package com.liuyi.springbootdemo.mybatisplus.controller;
 
 import com.liuyi.springbootdemo.mybatisplus.entity.User;
 import com.liuyi.springbootdemo.mybatisplus.mapper.UserMapper;
-import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +24,7 @@ public class MybatisPlusController {
     public void test(){
         System.out.println(("----- selectAll method test ------"));
         List<User> userList = userMapper.selectList(null);
-        Assert.assertEquals(5, userList.size());
+//        Assert.assertEquals(5, userList.size());
         userList.forEach(System.out::println);
     }
 }
